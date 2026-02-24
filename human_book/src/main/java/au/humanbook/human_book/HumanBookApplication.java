6fb5edee67575002ec4da7819046daadb5dfae4b
MatthewAlexander2002 <matthewalexander009@gmail.com>
2026-02-25 10:19:13 +1100

    setup and running

---
+++ human_book/src/main/java/au/humanbook/human_book/HumanBookApplication.java
@@ -0,0 +1,14 @@
+package au.humanbook.human_book;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HumanBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HumanBookApplication.class, args);
+		System.out.println("Hello World");
+	}
+
+}
---
+++ human_book/src/test/java/au/humanbook/human_book/HumanBookApplicationTests.java
@@ -0,0 +1,13 @@
+package au.humanbook.human_book;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HumanBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
